21e4afcbd199e79558843e851460a5993f6ced3a
李一航 <liyihang@corp.netease.com>
2019-09-24 13:06:45 +0800

    add .gitignore

--- Command/command.py
+++ Command/command.py
@@ -1,5 +1,79 @@
-"""
-命令模式
+# -*- encoding: utf-8 -*-
+'''
+简单实现
+'''
 
-将一个请求封装为一个对象，从而使你可用不同的请求对客户进行参数化； 对请求排队或记录请求日志，以及支持可撤销的操作。
-"""
+
+def press(input):
+    print input
+
+
+def jump_action():
+    print "jump"
+
+
+def fire_gun_action():
+    print "fire gun"
+
+
+def swap_weapon_action():
+    print "swap weapon"
+
+
+def reload_bullet_action():
+    print "reload"
+
+
+def Input():
+    if press('x'):
+        jump_action()
+    elif press('y'):
+        fire_gun_action()
+    elif press('a'):
+        swap_weapon_action()
+    elif press('b'):
+        reload_bullet_action()
+
+
+'''
+配置按键
+个人思路:
+将变量和函数对象绑定
+'''
+
+
+class PressAction(object):
+    INSTANCE = {}
+
+    def __init__(self, press, action):
+        self.press = press
+        self.action = action
+        self.INSTANCE[self.press] = self
+
+    def reset_press(self, new_press):
+        self.INSTANCE.pop(self.press)
+        self.press = new_press
+        self.INSTANCE[self.press] = self
+
+    def execute(self):
+        self.action()
+
+
+'''
+全局初始化
+'''
+
+jump = PressAction('x', jump_action)
+fire_gun = PressAction('y', fire_gun_action)
+swap_weapon = PressAction('a', swap_weapon_action)
+reload_bullet = PressAction('b', reload_bullet_action)
+
+
+class InputHander(object):
+    def __init__(self):
+        self.press_actions = PressAction.INSTANCE
+
+    def input(self):
+        press_action = self.press_actions.get()
+        if press_action:
+            press_action.execute()
